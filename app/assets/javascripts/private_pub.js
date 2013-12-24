8007883203ff46f837384824e6c58fcaa234a67b
Sergey Hoh <sergh.84@gmail.com>
2013-12-24 14:18:44 +0200

    Add IE8

--- app/assets/javascripts/private_pub.js
+++ app/assets/javascripts/private_pub.js
@@ -1,97 +1,111 @@
 function buildPrivatePub(doc) {
-  var self = {
-    connecting: false,
-    fayeClient: null,
-    fayeCallbacks: [],
-    subscriptions: {},
-    subscriptionObjects: {},
-    subscriptionCallbacks: {},
+    var self = {
+        connecting: false,
+        fayeClient: null,
+        fayeCallbacks: [],
+        subscriptions: {},
+        subscriptionObjects: {},
+        subscriptionCallbacks: {},
 
-    faye: function(callback) {
-      if (self.fayeClient) {
-        callback(self.fayeClient);
-      } else {
-        self.fayeCallbacks.push(callback);
-        if (self.subscriptions.server && !self.connecting) {
-          self.connecting = true;
-          var script = doc.createElement("script");
-          script.type = "text/javascript";
-          script.src = self.subscriptions.server + ".js";
-          script.onload = self.connectToFaye;
-          doc.documentElement.appendChild(script);
+        faye: function(callback) {
+            if (self.fayeClient) {
+                callback(self.fayeClient);
+            } else {
+                self.fayeCallbacks.push(callback);
+                if (self.subscriptions.server && !self.connecting) {
+                    self.connecting = true;
+                    var script = doc.createElement("script");
+                    script.type = "text/javascript";
+                    script.src = self.subscriptions.server + ".js";
+                    var ieMatch = navigator.userAgent.match(/(msie) (\d+)/i);
+                    if(ieMatch && ieMatch[2] == 8){
+                        var done = false;
+                        script.onload = script.onreadystatechange = function(){
+                            if(!done && (!this.readyState || this.readyState === "loaded" || this.readyState === "complete")){
+                                done = true;
+                                self.connectToFaye();
+                                script.onload = script.onreadystatechange = null;
+                                doc.documentElement.appendChild(script);
+                            }
+                        }
+                    }else{
+                        script.onload = self.connectToFaye;
+                        doc.documentElement.appendChild(script);
+                    }
+                }
+            }
+        },
+
+        connectToFaye: function () {
+            self.fayeClient = new Faye.Client(self.subscriptions.server);
+            self.fayeClient.addExtension(self.fayeExtension);
+            for (var i = 0; i < self.fayeCallbacks.length; i++) {
+                self.fayeCallbacks[i](self.fayeClient);
+            }
+            ;
+        },
+
+        fayeExtension: {
+            outgoing: function (message, callback) {
+                if (message.channel == "/meta/subscribe") {
+                    // Attach the signature and timestamp to subscription messages
+                    var subscription = self.subscriptions[message.subscription];
+                    if (!message.ext) message.ext = {};
+                    message.ext.private_pub_signature = subscription.signature;
+                    message.ext.private_pub_timestamp = subscription.timestamp;
+                }
+                callback(message);
+            }
+        },
+
+        sign: function (options) {
+            if (!self.subscriptions.server) {
+                self.subscriptions.server = options.server;
+            }
+            self.subscriptions[options.channel] = options;
+            self.faye(function (faye) {
+                var sub = faye.subscribe(options.channel, self.handleResponse);
+                self.subscriptionObjects[options.channel] = sub;
+                if (options.subscription) {
+                    options.subscription(sub);
+                }
+            });
+        },
+
+        handleResponse: function (message) {
+            if (message.eval) {
+                eval(message.eval);
+            }
+            if (callback = self.subscriptionCallbacks[message.channel]) {
+                callback(message.data, message.channel);
+            }
+        },
+
+        subscription: function (channel) {
+            return self.subscriptionObjects[channel];
+        },
+
+        unsubscribeAll: function () {
+            for (var i in self.subscriptionObjects) {
+                if (self.subscriptionObjects.hasOwnProperty(i)) {
+                    self.unsubscribe(i);
+                }
+            }
+        },
+
+        unsubscribe: function (channel) {
+            var sub = self.subscription(channel);
+            if (sub) {
+                sub.cancel();
+                delete self.subscriptionObjects[channel];
+            }
+        },
+
+        subscribe: function (channel, callback) {
+            self.subscriptionCallbacks[channel] = callback;
         }
-      }
-    },
-
-    connectToFaye: function() {
-      self.fayeClient = new Faye.Client(self.subscriptions.server);
-      self.fayeClient.addExtension(self.fayeExtension);
-      for (var i=0; i < self.fayeCallbacks.length; i++) {
-        self.fayeCallbacks[i](self.fayeClient);
-      };
-    },
-
-    fayeExtension: {
-      outgoing: function(message, callback) {
-        if (message.channel == "/meta/subscribe") {
-          // Attach the signature and timestamp to subscription messages
-          var subscription = self.subscriptions[message.subscription];
-          if (!message.ext) message.ext = {};
-          message.ext.private_pub_signature = subscription.signature;
-          message.ext.private_pub_timestamp = subscription.timestamp;
-        }
-        callback(message);
-      }
-    },
-
-    sign: function(options) {
-      if (!self.subscriptions.server) {
-        self.subscriptions.server = options.server;
-      }
-      self.subscriptions[options.channel] = options;
-      self.faye(function(faye) {
-        var sub = faye.subscribe(options.channel, self.handleResponse);
-        self.subscriptionObjects[options.channel] = sub;
-        if (options.subscription) {
-          options.subscription(sub);
-        }
-      });
-    },
-
-    handleResponse: function(message) {
-      if (message.eval) {
-        eval(message.eval);
-      }
-      if (callback = self.subscriptionCallbacks[message.channel]) {
-        callback(message.data, message.channel);
-      }
-    },
-
-    subscription: function(channel) {
-      return self.subscriptionObjects[channel];
-    },
-
-    unsubscribeAll: function() {
-      for (var i in self.subscriptionObjects) {
-        if ( self.subscriptionObjects.hasOwnProperty(i) ) {
-          self.unsubscribe(i);
-        }
-      }
-    },
-
-    unsubscribe: function(channel) {
-      var sub = self.subscription(channel);
-      if (sub) {
-        sub.cancel();
-        delete self.subscriptionObjects[channel];
-      }
-    },
-
-    subscribe: function(channel, callback) {
-      self.subscriptionCallbacks[channel] = callback;
-    }
-  };
-  return self;
+    };
+    return self;
 }
 
 var PrivatePub = buildPrivatePub(document);
